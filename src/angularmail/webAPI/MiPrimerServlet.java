06fce0fd7b98267f1a8bcf65a8c5d9fb1435b5f3
rafaelMunoz77 <rafael.munoz77@gmail.com>
2020-10-06 09:58:03 +0200

    Modulo01 - Vídeo 16 - Creamos el primer JServlet

---
+++ src/angularmail/webAPI/MiPrimerServlet.java
@@ -0,0 +1,40 @@
+package angularmail.webAPI;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MiPrimerServlet
+ */
+@WebServlet("/MiPrimerServlet")
+public class MiPrimerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MiPrimerServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("GET - Hola Mundo");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("POST - Hola Mundo");
+	}
+
+}
