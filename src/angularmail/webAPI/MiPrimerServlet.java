389bc441dbcd60de602ac63ebfb5a184c647136d
rafaelMunoz77 <rafael.munoz77@gmail.com>
2020-10-06 22:26:13 +0200

    Vídeo 18 - Web.xml y duplicidad de URLMappings

--- src/angularmail/webAPI/MiPrimerServlet.java
+++ src/angularmail/webAPI/MiPrimerServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MiPrimerServlet
  */
-@WebServlet("/MiPrimerServlet")
+//@WebServlet("/MiPrimerServlet")
 public class MiPrimerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
