2c0d576b85180f895a472a646054f1073c58d1cf
rafaelMunoz77 <rafael.munoz77@gmail.com>
2020-10-11 18:51:07 +0200

    Vídeo 25 - Seguridad por JWT

--- src/angularmail/webAPI/GetListadoMensajes.java
+++ src/angularmail/webAPI/GetListadoMensajes.java
@@ -37,32 +37,37 @@ public class GetListadoMensajes extends HttpServlet {
        // Necesito un mapper para trabajar con JSON
 		ObjectMapper mapper = new ObjectMapper();
 
-		// Obtengo el usuario autenticado y guardado en la sesión.
-		// Si no hay usuario no obtengo ni devuelvo listado de mensajes
-		Usuario u = (Usuario) request.getSession().getAttribute("usuarioAutenticado");
-		
-		if (u != null) {
-			// Obtengo la lista de mensajes del controlador JPA
-			List<Mensaje> mensajes = MensajeControlador.getControlador().findMensajesBandejaEntradaDeUsuario(u.getId());
-			
-			// Transformo la lista de mensajes en una lista de objetos DTO, que se transformará en JSON
-			List<DTO> mensajesDTO = new ArrayList<DTO>();
-			for (Mensaje m : mensajes) {
-				DTO dto = new DTO();
-				dto.put("id", m.getId());
-				dto.put("asunto", m.getAsunto());
-				dto.put("fecha", m.getFecha());
+		// si existe una cabecera de autenticación en el request, con nombre "Authentication", leo el JWT, que vendrá tras
+		// los caracteres "Bearer ". Tomo el JWT y obtengo el usuario autenticado gracias al id encriptado en su interior
+		if (request.getHeader("Authorization") != null) {
+			// Obtengo la cabecera del request, etiquetada como "Autorization", ahí debe llegar el JWT del usuario autenticado
+			String stringAuthorization = request.getHeader("Authorization");
+			String jwt = stringAuthorization.substring(7); // Eliminamos la cabecera "Bearer ", antes del jwt			
+
+			Usuario usuAutenticado = AutenticadorJWT.cargaUsuarioDesdeJWT(jwt);
+
+			if (usuAutenticado != null) {
+				// Obtengo la lista de mensajes del controlador JPA
+				List<Mensaje> mensajes = MensajeControlador.getControlador().findMensajesBandejaEntradaDeUsuario(usuAutenticado.getId());
 				
-				mensajesDTO.add(dto); // Agrego el nuevo dto creado a la lista de dtos que van a viajar al cliente
+				// Transformo la lista de mensajes en una lista de objetos DTO, que se transformará en JSON
+				List<DTO> mensajesDTO = new ArrayList<DTO>();
+				for (Mensaje m : mensajes) {
+					DTO dto = new DTO();
+					dto.put("id", m.getId());
+					dto.put("asunto", m.getAsunto());
+					dto.put("fecha", m.getFecha());
+					
+					mensajesDTO.add(dto); // Agrego el nuevo dto creado a la lista de dtos que van a viajar al cliente
+				}
+				
+				// Devuelvo, en formato JSON, la lista de mensajes
+				response.getWriter().append(mapper.writeValueAsString(mensajesDTO));
+				return;
 			}
-			
-			// Devuelvo, en formato JSON, la lista de mensajes
-			response.getWriter().append(mapper.writeValueAsString(mensajesDTO));
+
 		}
-		else {
-			// En caso de que no haya usuario autenticado, devuelvo nulo, advirtiendo del error.
-			response.getWriter().append(null);
-		}
-			
+		// En caso de que no haya usuario autenticado, devuelvo nulo, advirtiendo del error.
+		response.getWriter().append(null);
 	}
 }
